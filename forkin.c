3276da16e43b1361fc7a3775ebde008dd7e89a7e
Othman Bichouna <obichouna@stuy.edu>
2017-11-16 20:13:54 -0500

    Changed some things

--- forkin.c
+++ forkin.c
@@ -5,19 +5,24 @@
 
 
 int main(){
+  int pids[2];
+  int i;
   srand(time(NULL));
   printf("Hi! I am the parent!\n");
-  int f = fork();
-  fork();
 
-  if(!f){
-    printf("\tHi! I am a child with pid: %d\n", getpid());
-    int r = (rand() % 20) + 5;
-    sleep(r);
-    printf("\t I just slept, that was a good nap!\n");
-  }else{
-    
+  for(i = 0; i < 2; i++){
+    pids[i] = fork();
+    if(!(pids[i]))){
+      printf("\tHi! I am a child with pid: %d\n", getpid());
+      int r = (rand() % 20) + 5;
+      sleep(r);
+      printf("\t I just slept, that was a good nap!\n");
+      exit(0);
+    }
   }
-
+  
+  int status;
+  int pid;
+  while(n
 
 }
